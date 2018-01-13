8f7e60500e7fc2b19a7317dcc51c9fd22bc1bb96
olaliz <35194213+olaliz@users.noreply.github.com>
2018-01-13 12:57:32 -0600

    Add files via upload

---
+++ users.js
@@ -0,0 +1,57 @@
+'use strict';
+
+
+module.exports = function(_, passport, User){
+
+      return {
+         //get routing
+         SetRouting: function(router){
+            router.get('/', this.indexPage);
+            router.get('/signup', this.getSignUp);
+            router.get('/home', this.homePage);
+            router.get('/auth/facebook', this.getFacebookLogin);
+            router.get('/auth/facebook/callback', this.facebookLogin);
+            
+            
+            //post routing
+            router.post('/', User.LoginValidation, this.postLogin);
+            router.post('/signup', User.SignUpValidation, this.postSignUp);
+          },
+          
+          indexPage: function(req, res){
+          const errors = req.flash('error');
+              return res.render('index', {title: 'Chatting | Login', messages: errors, hasErrors: errors.length > 0});
+          },
+          
+           postLogin: passport.authenticate('local.login', {
+               successRedirect: '/home',
+               failureRedirect: '/',
+               failureFlash: true
+               }),
+               
+          getSignUp: function(req, res){
+               const errors = req.flash('error');
+               return res.render('signup', {title: 'Chatting | SignUp', messages: errors, hasErrors: errors.length > 0});
+          },
+          
+          postSignUp: passport.authenticate('local.signup', {
+               successRedirect: '/home',
+               failureRedirect: '/signup',
+               failureFlash: true
+               }),
+               
+         getFaceookLogin: passport.authenticate('facebook', {
+                scope: 'email'
+                }),
+                
+         facebookLogin: passport.authenticate('facebook', {
+                 successRedirect: '/home',
+                 failureRedirect: '/signup',
+                 failureFlash: true
+                 }),
+                 
+          homePage: function(req, res){
+                  return res.render('home');
+                  }
+      }
+}
